8739b00050d6fbd0d9129fe38944b529c48822d2
HecFer5 <hecalfer@hotmail.com>
2023-03-22 18:48:06 -0230

    usar variable de entorno

--- myApp.js
+++ myApp.js
@@ -6,11 +6,17 @@ module.exports = app;
 // rutaIndex = __dirname + "/views/index.html"
 // rutaEstilo = __dirname + "/public/"
 // app.use("/public",express.static(rutaEstilo));
+let mensaje = { "message": "Hello json" }
+let respuesta = ""
 
-app.get("/json", function (req, res) {
-    res.json({"message": "Hello json"});
+if (process.env.MESSAGE_STYLE === "uppercase") {
+    respuesta = mensaje.toUppercase
+}
+
+app.get("/", function (req, res) {
+    res.json(mensaje);
 });
- 
+
 
 
 
