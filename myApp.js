db0e9e2e7e54a4b3439a67df65ff8f45ae7a70a4
HecFer5 <hecalfer@hotmail.com>
2023-03-22 16:44:16 -0230

    leer saludo en ruta

--- myApp.js
+++ myApp.js
@@ -4,8 +4,11 @@ let app = express();
  module.exports = app;
 
  //console.log("Hello World")
+app.get("/", function(req, res) {
+  res.send("Hello Express");
+});
 
-app.get("Hello World")
+//app.get("Hello World")
 
 
 
